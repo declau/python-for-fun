ceed8191f260eb62a95c476946ec1a8ddf88191b
declau <dec82@hotmail.com>
2021-08-29 19:44:23 -0300

    feat: add while

--- secret_number.py
+++ secret_number.py
@@ -1,20 +1,27 @@
 numero_secreto = 21
+total_tentativas = 3
+tentativa = 1
 
-chute_str = input("Qual seu número: ")
-print("Você digitou ", chute_str)
+while total_tentativas >= tentativa:
 
-chute = int(chute_str)
+    print("Tentativa", tentativa, "de", total_tentativas)
 
+    chute_str = input("Qual seu número: ")
+    print("Você digitou ", chute_str)
 
-acerto = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    chute = int(chute_str)
 
-if acerto:
-    print("Você acertou!!!")
-elif maior:
-    print("Você errou, numero maior que o numero secreto")
-else:
-    print("Você errou, numero menor que o numero secreto")
+    acerto = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
+    if acerto:
+        print("Você acertou!!!")
+        break
+    elif maior:
+        print("Você errou, numero maior que o numero secreto")
+    else:
+        print("Você errou, numero menor que o numero secreto")
+    # total_tentativas = total_tentativas - 1
+    tentativa = tentativa + 1
 print("Fim de jogo")
