0e245c3f2901870a3e5a18a7b63d740bb7392bce
declau <dec82@hotmail.com>
2021-09-06 19:30:19 -0300

    feat: Add methods to class

--- oo/conta.py
+++ oo/conta.py
@@ -4,3 +4,12 @@ class Conta:
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+    def extrato(self):
+        print(f"Saldo de {self.saldo} do {self.titular}")
