f36057e94ccae7b61bb873da682f6814b4f88208
declau <dec82@hotmail.com>
2021-09-08 17:43:41 -0300

    feat: private attributes

--- oo/conta.py
+++ oo/conta.py
@@ -1,15 +1,15 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
-
-    def deposita(self, valor):
-        self.saldo += valor
-
-    def saca(self, valor):
-        self.saldo -= valor
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print(f"Saldo de {self.saldo} do {self.titular}")
+        print(f"Saldo de {self.__saldo} do {self.__titular}")
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
