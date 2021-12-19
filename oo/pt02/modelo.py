b69b970d3e567f2f86560a7a312a234165d27bd6
declau <dec82@hotmail.com>
2021-12-19 18:22:03 -0300

    feat: reusing a list

--- oo/pt02/modelo.py
+++ oo/pt02/modelo.py
@@ -38,14 +38,12 @@ class Serie(Programa):
     def __str__(self):
          return f"{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes"
     
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
+        super().__init__(programas)
         self.nome = nome.title()
         self.programas = programas
 
-    def tamanho(self):
-        return len(self.programas)
-         
 
 vingadores = Filme("vingadores - guerra infinita", 2018, 160)
 mad_max = Filme("mad max - estrada da furia", 2016, 185)
@@ -66,6 +64,8 @@ justiceiro.dar_like()
 filmes_series = [vingadores, mad_max, atlanta, justiceiro]
 playlist_fim_semana = Playlist("playlist fim de semana", filmes_series)
 
-for programa in playlist_fim_semana.programas:
+print( f"Quantidade de Filmes ou Series: {len(playlist_fim_semana)}")
+for programa in playlist_fim_semana:
     print(programa)
-print( f"Quantidade de Filmes ou Series: {playlist_fim_semana.tamanho()}")
+
+print(f"Faz parte da Playlist? {justiceiro in playlist_fim_semana}")
