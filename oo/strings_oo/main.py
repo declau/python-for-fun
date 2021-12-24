33077512fb0b285ccb8b9f6e3c16f852c37503dc
declau <dec82@hotmail.com>
2021-12-24 13:34:41 -0300

    feat: refactor

--- oo/strings_oo/main.py
+++ oo/strings_oo/main.py
@@ -1,24 +1,19 @@
-url = "bytebank.com/cambio?moedaOrigem=real"
-print(url)
-#pega a base da url
-url_base = url[0 : 19]
-print(url_base, "URL base")
+url = "bytebank.com/cambio?quantidade=100&moedaOrigem=real&moedaDestino=dolar"
 
-#pega somente os parametros da url
-url_parametros = url[20:]
-url_parametros = url[20:36]
-print(url_parametros, "URL parametros")
+# Separa base e parâmetros
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
 
-#string no Python são imutáveis
-print(url, "padrão")
+# Busca o valor de um parâmetro
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
 
-
-# nova URL
-nova_url = "http://bytebank.com/cambio?moedaOrigem=real"
-#find retorna posição na string
-indice_interrogacao = nova_url.find("?")
-url_find = nova_url[0:indice_interrogacao]
-print(url_find, "URL base find")
-
-url_find_parametro = nova_url[indice_interrogacao+1:]
-print(url_find_parametro, "URL parametro find")
+print(valor)
