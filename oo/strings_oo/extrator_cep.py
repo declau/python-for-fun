1b1af509aa7f5584fde8100fbca079df17ae590a
declau <dec82@hotmail.com>
2021-12-27 17:14:52 -0300

    feat: adjust regex

--- oo/strings_oo/extrator_cep.py
+++ oo/strings_oo/extrator_cep.py
@@ -1,8 +1,8 @@
-endereco = "Rua dos Bobos 00, apartamento 00, Lugar Nenhum, Poços de Caldas, MG, 37707000"
+endereco = "Rua dos Bobos 00, apartamento 00, Lugar Nenhum, Poços de Caldas, MG, 37707-000"
 
 import re # Regular Expression
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 busca = padrao.search(endereco) # Match
 
 if busca:
